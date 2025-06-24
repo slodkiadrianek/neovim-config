f5e93045ee1dba0af53ee922f832dd3764bc5c19
Adrian Kurek <adikurek11@gmail.com>
2025-06-24 17:50:59 +0200

    CURSOR

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
-vim.opt.guicursor = {
-  "n-v-c:block", -- block cursor in normal/visual/command modes
-  "i-ci-ve:ver35", -- vertical bar (35% width) in insert and related modes
-  "r-cr:hor20", -- horizontal bar in replace mode
-  "o:hor50", -- wide horizontal bar in operator-pending mode
-}
+-- Set block cursor for all modes
+vim.opt.guicursor = ""
+-- vim.opt.guicursor = {
+--   "n-v-c:block", -- block cursor in normal/visual/command modes
+--   "i-ci-ve:ver35", -- vertical bar (35% width) in insert and related modes
+--   "r-cr:hor20", -- horizontal bar in replace mode
+--   "o:hor50", -- wide horizontal bar in operator-pending mode
+-- }
